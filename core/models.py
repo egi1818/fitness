d7032ec06650b1a926348f6189f793a2960acf95
egi1818 <egibeno896@gmail.com>
2022-09-11 19:10:58 +0530

    profile

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     subs = [
@@ -15,6 +17,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+	instance.profile.save()
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
